2e31548d4f2e75be5022715eed14e399c3cb4f5c
Asghar mohammadi <AsgharMohammadi313@email.com>
2026-09-19 18:49:58 +0430

    Customize portfolio for Asghar Mohammadi

--- resources/js/client/admin/components/layout/NavContent.js
+++ resources/js/client/admin/components/layout/NavContent.js
@@ -38,6 +38,7 @@ const NavContent = () => {
                 {
                     demoMode && (
                         <ItemWrapper className="z-hover">
+                 
                             <a href='https://github.com/arifszn/ezfolio' target="_blank" rel="noreferrer" title="Source">
                                 <Avatar /* style={{ backgroundColor: 'var(--primary-hover-color)' }} */ icon={<GithubOutlined />} size="small"/>
                             </a>
